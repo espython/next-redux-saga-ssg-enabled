1421ce19715de1a45f49153388e6f5cf27f2d58d
esnode85 <esnodejs@gmail.com>
2020-04-24 00:14:06 +0200

    setup the project successfully

---
+++ redux/store.js
@@ -0,0 +1,37 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+// export function initializeStore(initialState = {}) {
+//   return createStore(
+//     rootReducer,
+//     initialState,
+//     composeWithDevTools(applyMiddleware(thunk))
+//   );
+// }
+
+const bindMiddleware = (middleware = []) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension');
+    const { logger } = require(`redux-logger`);
+    middleware.push(logger);
+    return composeWithDevTools(applyMiddleware(...middleware));
+  }
+  return applyMiddleware(...middleware);
+};
+
+function configureStore(initialState = {}) {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    rootReducer,
+    initialState,
+    bindMiddleware([sagaMiddleware])
+  );
+
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+
+  return store;
+}
+
+export default configureStore;
